fe137a506ca5a106f49e87593ee8bdfc897a798a
cecilia-lim <cjl2235@columbia.edu>
2024-05-08 22:14:23 -0400

    media alt, larger text, drag-drop

--- server.py
+++ server.py
@@ -15,36 +15,42 @@ lessons = [
         "title": "Part 1: Basic Terminology",
         "text": """Let's start with some basic terms! A chord is any group of two or more notes played together. Depending on the notes that make up the chord and the quality of sound they produce, there are different names for each one. You likely have heard of major and minor chords, where major chords sound bright & happy, and minor chords are sad or contemplative. Look below at different chords!""",
         "img":"https://wpe.hoffmanacademy.com/wp-content/uploads/2021/02/How-to-Read-and-Play-Piano-Chords.jpeg",
+        "alt":"three chords on a staff",
     },
     {
         "id": 2,
         "title": "Part 1: Chord Progression",
-        "text": """A chord progression is simply a sequence of chords. Oftentimes pop songs have a sequence of 4 chords that repeat throughout the song. Pop songs are notorious for recycling chord progressions. We represent different chord progressions as a string of roman numerals. They may seem intimidating, but for now, just know that the numbers help us understand the chords in relation to each other. In each song, we have different expectations for which chords will be major or minor, without even realizing it! Major chords are represented by capital roman numerals, while minor chords are in lowercase. Below are some common chord progressions and their notation!""",
+        "text": """A chord progression is simply a sequence of chords. Oftentimes pop songs have a sequence of 4 chords that repeat throughout the song. We represent different chord progressions as a string of roman numerals. In each song, we have different expectations for which chords will be major or minor, without even realizing it! Major chords are represented by capital roman numerals, while minor chords are in lowercase. Below are some common chord progressions and their notation!""",
         "img":"https://writetracks.files.wordpress.com/2012/11/chord_progressions.jpg",
+        "alt":"chord progression numerals",
     },
     {
         "id": 3,
         "title": "Part 2: The I-iii-vi-IV",
         "text": """Let's look at a specific chord progression: the 1-3-6-4. Often known as the “emotional progression,” many pop and rock ballads use this formula. Click the play button to hear the progression.""",
-        "video":"https://www.youtube.com/embed/eu__YcNCDgY"
+        "video":"https://www.youtube.com/embed/eu__YcNCDgY",
+        "alt":"I-iii-vi-IV",
     },
     {
         "id": 4,
         "title": "Part 2: Traitor",
-        "text": "You might recognize the chord progression from Olivia Rodrigo's song 'Traitor'.",
-        "video":"https://www.youtube.com/embed/6tsu2oeZJgo"
+        "text": "You might recognize the chord progression from Olivia Rodrigo's song 'Traitor'. (You may have to click 'Watch on Youtube' due to copyright reasons)",
+        "video":"https://www.youtube.com/embed/E2NuSNZNKnM",
+        "alt":"Traitor by Olivia Rodrigo",
     },
     {
         "id": 5,
         "title": "Part 3: I-III-IV-iv",
         "text":"Let's look at another progression. This one does something a little funky: it adds two chords that aren't from the key: E major (the III) and F minor (the iv).",
-        "video":"https://www.youtube.com/embed/Ud5CkdeL3K4"
+        "video":"https://www.youtube.com/embed/Ud5CkdeL3K4",
+        "alt":"I-III-IV-iv",
     },
     {
         "id": 6,
         "title": "Part 3: Vampire",
-        "text": "You might recognize the chord progression from Olivia Rodrigo's song 'Vampire'. It works well with the song because the unexpected sounds add an eerie quality!",
-        "video":"https://www.youtube.com/embed/RlPNh_PBZb4"
+        "text": "You might recognize the chord progression from Olivia Rodrigo's song 'Vampire'. It works well with the song because the unexpected sounds add an eerie quality! (You may have to click 'Watch on Youtube' due to copyright reasons)",
+        "video": "https://www.youtube.com/embed/aHmg0jsmNhg",
+        "alt":"Vampire by Olivia Rodrigo"
     },
 ]
 
@@ -105,9 +111,9 @@ questions = [
     },
     {
         "id": 5,
-        "text": """Which chord progression do the songs use?""",
-        "options": {1:"https://i.discogs.com/Rd83pLBgs7speREI6__eXegcuCbhIm1qh3ZXuAmb5yI/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI3NTUy/NjkwLTE2ODg0MzY5/NzItNzY1NC5qcGVn.jpeg", 
-                    2:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7CcGIjS-p3_0BvsP2BIrJ42Enk7ioQaLgMXNXXWtMKQ&s"},
+        "text": """Match each song to its chord progression (left: Traitor, right: Vampire):""",
+        "options": {1:"https://upload.wikimedia.org/wikipedia/en/b/b2/Olivia_Rodrigo_-_SOUR.png",
+                    2: "https://i.discogs.com/Rd83pLBgs7speREI6__eXegcuCbhIm1qh3ZXuAmb5yI/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI3NTUy/NjkwLTE2ODg0MzY5/NzItNzY1NC5qcGVn.jpeg",},
         "answer": ""
     },
 ]
